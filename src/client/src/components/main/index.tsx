7f2ed497c20816f479df7365767df4281f08c38d
Kevin Burk <percburk@gmail.com>
2022-12-17 15:23:52 -0600

    Update dependency versions, add tailwind and config, tailwind prettier extension

--- src/client/src/components/main/index.tsx
+++ src/client/src/components/main/index.tsx
@@ -31,6 +31,7 @@ export const Main = () => {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
 }
